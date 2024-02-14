91a487aeddb29b3dd90f9d269f4556f1a64a2ff7
Adwait Shinganwade <amshinganwade@gmail.com>
2024-02-14 13:16:04 -0700

    Implemented strategy for investment. To finish strategy for attack

--- Assignment 2 - Economancy Player/economancy_player.hs
+++ Assignment 2 - Economancy Player/economancy_player.hs
@@ -5,7 +5,7 @@ import Data.Map (Map, fromList)
 import qualified Data.Map as Map
 
 referenceCards :: Map String Card =
-  Map.fromList [ 
+  Map.fromList [
     ("Sorcerer's Stipend", Card{name="Sorcerer's Stipend", uses=0, attack=0, defense=0, victory_points=0, cost=0}),
     ("Board of Monopoly", Card{name="Board of Monopoly", uses=0, attack=1, defense=1, victory_points=1, cost=2}),
     ("Incantation", Card{name="Incantation", uses=0, attack=1, defense=1, victory_points=3, cost=4}),
@@ -17,10 +17,9 @@ referenceCards :: Map String Card =
     ("Gold Fish", Card{name="Gold Fish", uses=0, attack=1, defense=2, victory_points=0, cost=0})
   ]
 
--- TODO - earning per day, separate function of day and card?
-    
-data Card 
-    = Card 
+
+data Card
+    = Card
     {
         name :: String,
         uses :: Int,
@@ -29,24 +28,24 @@ data Card
         victory_points :: Int,
         cost :: Int
     }
-    deriving (Show)
+    deriving (Show, Eq)
 
 data Player = Player
   { coins :: Int,
     buys :: Int,
     cards :: [Card]
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data Phase
-  = InvestingOrBuy {name :: String}
+  = InvestingOrBuy {phaseName :: String}
     | Attacking
-      { name :: String,
+      { phaseName :: String,
         attacker :: Int,
         attacker_card :: Maybe Int
       }
     | End {
-        name :: String,
+        phaseName :: String,
         winner :: Maybe Int}
     deriving (Show)
 
@@ -58,3 +57,44 @@ data State = State
     player :: Int
   }
   deriving (Show)
+
+
+-- Gets the amount of coins the player wishes to invest, which will be
+-- between 0 and the maximum coins the player has. The second paramter
+-- is a random number in [0, coins player]
+getInvestment :: State -> Int -> Int
+getInvestment state randomInvestment =
+  -- If the total attacking potential is less than that of the total 
+  -- attacking potential of half the remaining players, invest all coins
+  let
+    allButThisPlayer = [totalAttackingPotential p | p <- players state,  p /= thisPlayer]
+    strongerThanPlayer = numberOfStrongerOpponents (totalAttackingPotential thisPlayer) allButThisPlayer
+  in if strongerThanPlayer >= length allButThisPlayer `div` 2
+    then coins thisPlayer
+    else randomInvestment
+    where thisPlayer = players state !! player state
+
+totalAttackingPotential :: Player -> Int
+totalAttackingPotential player =
+  sum [attack (referenceCards Map.! (name :: Card -> String) card) | card <- cards player]
+
+
+numberOfStrongerOpponents :: (Foldable t, Ord a, Num b) => a -> t a -> b
+numberOfStrongerOpponents strongerThan = foldl (\x y -> if y > strongerThan then x+1 else x) 0
+
+-- Chooses the strongest untapped card
+chooseAttackingCard :: State -> Int
+chooseAttackingCard state =
+  let playerCards = cards thisPlayer
+      strongestCard = getStrongestAttackingCard playerCards 
+      in strongestCard
+      where thisPlayer = players state !! player state
+
+
+-- Get strongest attacking card by index (maybe later balance attack and defense?)
+getStrongestAttackingCard :: [Card] -> Int
+getStrongestAttackingCard deck =
+  let enumeratedDeck = zip [0..(length deck - 1)] deck
+  in
+  -- No Wall of Wealth card yet, hence based around uses == 0
+  fst (foldl (\x y -> if uses (snd y) == 0 && attack (snd y) > attack (snd x) then y else x) (0, head deck) enumeratedDeck) 
